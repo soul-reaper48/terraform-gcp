d34142676ea2885905589dbbce5277a47adf83cc
soul-reaper48 <vikas.gokhale.v@gmail.com>
2021-08-24 14:12:55 +0530

    compute-instance

--- network/main.tf
+++ network/main.tf
@@ -22,10 +22,11 @@ resource "google_compute_network" "vpc_network" {
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "f1-micro"
+  tags         = ["web", "dev"]
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "cos-cloud/cos-stable"
     }
   }
 
